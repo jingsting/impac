9fcdb7bd4e488709265854146b1e88da0b027598
Ethan Lynn <ethan@sabado.com>
2019-03-21 18:04:56 -0400

    Restructuring and documentation for simple packer

--- src/region.hpp
+++ src/region.hpp
@@ -1,5 +1,5 @@
-#ifndef IMPAC_region_HPP
-#define IMPAC_region_HPP
+#ifndef IMPAC_REGION_HPP
+#define IMPAC_REGION_HPP
 
 #include <algorithm>
 #include <cassert>
@@ -7,9 +7,9 @@
 #include <type_traits>
 
 namespace impac {
-    // Templated rectangle class used by the packing algorithm to define a
-    // (x, y, width, height) region of an image. The template parameter T should
-    // be an arithmetic type. Width and height are always >= 0.
+    // Describes a (x, y, width, height) identified by a tag value. Used by
+    // the packer classes to associate a given image to a placement within a
+    // texture atlas.
     template<typename T, typename U>
     class region {
     public:
@@ -18,21 +18,20 @@ namespace impac {
         // No default constructor
         region() = delete;
 
-        // Default copy/move semantics.
+        // Default copy/move semantics
         region(const type&) = default;
         region& operator=(const type&) = default;
         region(type&&) = default;
         region& operator=(type&&) = default;
 
-        // Construct a region with a tag, x, y, width, and height
+        // Constructs a region with a tag, x, y, width, and height
         region(T tag, U x, U y, U width, U height)
             : tag_{tag}, x_{x}, y_{y}, width_{width}, height_{height} 
         {
-            assert(width_ >= 0);
-            assert(height_ >= 0);
+            assert(width_ >= 0 && height_ >= 0);
         }
 
-        // Determines if the given region intersects with this.
+        // Determines if the given region intersects
         bool intersects(const type& that)
         {
             return (
@@ -40,20 +39,64 @@ namespace impac {
                 that.y_ < y_ + height_ && y_ < that.y_ + that.height_);
         }
 
-        // Computes the area of the region.
+        // Calculates the area
         U area() const { return width_ * height_; }
 
+        // Gets the tag
         const T& tag() const { return tag_; }
+
+        // Gets the x/left position
         U x() const { return x_; }
+
+        // Gets the y/top position
         U y() const { return y_; }
+
+        // Gets the width
         U width() const { return width_; }
+
+        // Gets the height
         U height() const { return height_; }
+
+        // Gets the right position (left + width)
         U right() const { return x_ + width_; }
+
+        // Gets the bottom position (top + height)
         U bottom() const { return y_ + height_; }
 
+        // Sets the x/left position
         void set_x(U x) { x_ = x; }
+
+        // Sets the y/top position
         void set_y(U y) { y_ = y; }
-        void set_position(U x, U y) { x_ = x; y_ = y; }
+
+        // Sets the (x, y)/(top, left) position
+        void set_position(U x, U y)
+        {
+            x_ = x;
+            y_ = y;
+        }
+
+        // Sets the width
+        void set_width(U width)
+        {
+            width_ = width;
+            assert(width_ >= 0);
+        }
+
+        // Sets the height
+        void set_height(U height)
+        {
+            height_ = height;
+            assert(height_ >= 0);
+        }
+
+        // Sets the dimensions
+        void set_size(U width, U height)
+        {
+            width_ = width;
+            height_ = height;
+            assert(width_ >= 0 && height_ >= 0);
+        }
     private:
         T tag_;     // tag used to identify region
         U x_;       // left x-position of the region
--- src/simple_packer.cpp
+++ src/simple_packer.cpp
@@ -1,23 +1,23 @@
 #include <algorithm>
 #include <cassert>
-#include "simple.hpp"
+#include "simple_packer.hpp"
 
 namespace impac {
     simple_packer::simple_packer(
         std::vector<region_type>&& regions,
-        int max_width,
-        int max_height)
+        int available_width,
+        int available_height)
         : regions_{regions},
-          max_width_{max_width},
-          max_height_{max_height}
+          available_width_{available_width},
+          available_height_{available_height}
     {
-        assert(max_width_ >= 0);
-        assert(max_height_ >= 0);
+        assert(available_width_ >= 0);
+        assert(available_height_ >= 0);
     }
 
     bool simple_packer::pack(int& total_width, int& total_height)
     {
-        // Sort regions by descending order of area.
+        // Sort regions by descending order of area
         std::sort(
             regions_.begin(),
             regions_.end(),
@@ -32,32 +32,34 @@ namespace impac {
         auto r = &regions_[i];  // region currently being placed
 
         // Scan through available space
-        for (auto y = int{0}; y < max_height_; ++y) {
-            // If vertical space isn't adequate, terminate loop.
-            if (y + r->height() > max_height_)
+        for (auto y = int{0}; y < available_height_; ++y) {
+            // If vertical space isn't adequate, packing failed
+            if (y + r->height() > available_height_)
                 break;
-            for (auto x = int{0}; x < max_width_; ++x) {
-                // If horizontal space left isn't adequate, go to next row.
-                if (x + r->width() > max_width_)
+            for (auto x = int{0}; x < available_width_; ++x) {
+                // If horizontal space isn't adequate, move to next row
+                if (x + r->width() > available_width_)
                     break;
-                // Check if region intersects with any previously placed region.
+                // Check if region intersects with any previously placed region
                 auto placed = true;
                 r->set_position(x, y);
-                for (auto j = unsigned{0}; j < i; ++j)
+                for (unsigned j = 0; j < i; ++j)
                     if (r->intersects(regions_[j]))
                         placed = false;
                 if (placed) {
-                    // Update the total width & total height consumed
+                    // Update the total width/height consumed
                     total_width = std::max(total_width, r->right());
                     total_height = std::max(total_height, r->bottom());
-                    // If all regions are placed, indicate success.
+                    // If all regions are placed, indicate success
                     if (++i == regions_.size())
                         return true;
+                    // Shift by the consumed space and select next region
                     x += r->width();
                     r = &regions_[i];
                 }
             }
         }
+        // Ran out of space, packing failed
         return false;
     }
 }
