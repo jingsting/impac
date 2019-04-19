a39bc11cb8673c970baa2eaf43a9b16e4887a031
Ethan Lynn <ethan@sabado.com>
2019-04-19 03:50:45 -0400

    Modfied ofstream stuff to use const char*

--- src/impac.cpp
+++ src/impac.cpp
@@ -189,7 +189,7 @@ int main(int argc, const char** argv)
     image.write(image_path.native());
 
     // Generate the output data and save it to the output path.
-    auto data = std::ofstream{data_path.native()};
+    auto data = std::ofstream{data_path.c_str()};
     if (data_path.extension() == ".json") {
         data << json(sprite_sheet);
     } else if (data_path.extension() == ".xml") {
