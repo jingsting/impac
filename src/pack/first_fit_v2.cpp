e11b13218f8f99523dba1c6d4ccbd3381281535a
Jingfei Zhou <jingfei1024@gmail.com>
2019-04-26 18:53:36 -0400

    fixed v2

--- src/pack/first_fit_v2.cpp
+++ src/pack/first_fit_v2.cpp
@@ -27,16 +27,17 @@ bool impac::pack::first_fit_v2(sprite_sheet& sprite_sheet)
     // #####################################################################
     size_t canvasw = sprite_sheet.width();
     size_t canvash = sprite_sheet.height();
-    std::vector <std::vector<size_t> > canvas(canvash, std::vector<size_t>(canvasw, 0));
+    std::vector<std::vector<size_t> > canvas(canvash, std::vector<size_t>(canvasw, 0));
     for (auto i = size_t{0}; i < sprites.size(); ++i) {
         auto found = false;
         auto sprite = sprites[i];
-        // auto max_x = sprite_sheet.width() - sprite->width();
+        if (sprite_sheet.height() < sprite->height() ||
+                sprite_sheet.width() < sprite->width()) {
+            return false;
+        }
         auto max_y = sprite_sheet.height() - sprite->height();
-        
         for (auto h = size_t{0}; h <= max_y; ++h) {    
             auto wfits = 0;
-            auto found = false;
             for (auto x = size_t{0}; x < sprite_sheet.width(); ++x) {
                 auto lfits = true;
                 for (auto y = h; y < (h+sprite->height()); ++y) {
@@ -56,13 +57,15 @@ bool impac::pack::first_fit_v2(sprite_sheet& sprite_sheet)
                     auto startx = x+1-sprite->width();
                     auto starty = h;
                     sprite->set_position(startx, starty);
-                    for (auto w = startx; w<startx+sprite->width(); ++w) {
-                        for (auto h = starty; h<starty+sprite->height(); ++h) {
+                    for (auto w = startx; w<(startx+sprite->width()); ++w) {
+                        for (auto h = starty; h<(starty+sprite->height()-1); ++h) {
                             canvas[w][h] = 1;
                         }
                     }
+                    break;
                 }
             }
+            if (found == true) break;
         }
         if (found == false) {
             return false;
